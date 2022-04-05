8c686b37fadf8497103c11f482bb0a2e60a55e85
Perry Jorss <pjorss123@gmail.com>
2022-04-05 16:22:16 -0400

    commit tree 9a5e67aa2a366ea3f408a9a5619ae6f22b9f805b

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Perry Jorss';
+const welcomeMessage = 'Welcome to Flatbook, Perry Jorss!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
